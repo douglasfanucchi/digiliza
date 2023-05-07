bb421dedd4d36f0c81b4b8123fa2c6493480d19b
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-05-07 12:28:56 -0300

    adds login route

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+	public function authenticate(Request $request)
+	{
+		$credentials = $request->validate([
+			'email' => ['required', 'email'],
+			'password' => ['required']
+		]);
+
+		if (Auth::attempt($credentials)) {
+			$request->session()->regenerate();
+
+			return response()->json(['success' => true]);
+		}
+
+		return response()->json(['success' => false], 400);
+	}
+}
